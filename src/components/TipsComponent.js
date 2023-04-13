0c7d0c6d8511833de4b07099e412394463f5b466
Umar Ahmed <108314943+umar-ahmed1@users.noreply.github.com>
2023-04-12 19:46:14 -0600

    started tips component

--- src/components/TipsComponent.js
+++ src/components/TipsComponent.js
@@ -37,26 +37,61 @@ export default function TipsComponent() {
         </Flex>
 
 
-      <Stack width='100%' spacing={5}>
+      <Stack width='100%' spacing={5} fontSize={{base:'13px',sm:'14px',md:'15px'}}>
         <Flex height='120px' width='100%' align='center'>
           <Flex width='100px' height='90%' borderRadius='full' border='1px solid grey' align='center' justify='center' mr={3} display={{base:'none',md:'flex'}}>1</Flex>
-          <Box backgroundColor='red.200' flexGrow={1} height='100%' borderRadius={10}></Box>
+          <Box backgroundColor='red.200' flexGrow={1} height='100%' borderRadius={10} align='center' justify='center'>
+            <Flex width='100%' justify='center' align='center' direction='column' p={1}>
+              <Text fontWeight={600}>Avoid using names or words found in the dictionary</Text>
+              <Text>jack</Text>
+              <Text>orange</Text>
+              <Text>football</Text>
+            </Flex>
+          </Box>
         </Flex>
         <Flex height='120px' width='100%' align='center'>
-          <Box backgroundColor='yellow.200' flexGrow={1} height='100%' borderRadius={10} mr={3}></Box>
+          <Box backgroundColor='yellow.200' flexGrow={1} height='100%' borderRadius={10} mr={3} align='center' justify='center'>
+            <Flex width='100%' justify='center' align='center' direction='column' p={1}>
+              <Text fontWeight={600}>Avoid using names or words found in the dictionary</Text>
+              <Text>jack</Text>
+              <Text>orange</Text>
+              <Text>football</Text>
+            </Flex>
+          </Box>
           <Flex width='100px' height='90%' borderRadius='full' border='1px solid grey' align='center' justify='center' display={{base:'none',md:'flex'}}>2</Flex>
         </Flex>
         <Flex height='120px' width='100%' align='center'>
           <Flex width='100px' height='90%' borderRadius='full' border='1px solid grey' align='center' justify='center' mr={3} display={{base:'none',md:'flex'}}>3</Flex>
-          <Box backgroundColor='orange.200' flexGrow={1} height='100%' borderRadius={10}></Box>
+          <Box backgroundColor='orange.200' flexGrow={1} height='100%' borderRadius={10} align='center' justify='center'>
+            <Flex width='100%' justify='center' align='center' direction='column' p={1}>
+              <Text fontWeight={600}>Avoid using names or words found in the dictionary</Text>
+              <Text>jack</Text>
+              <Text>orange</Text>
+              <Text>football</Text>
+            </Flex>
+          </Box>
         </Flex>
         <Flex height='120px' width='100%' align='center'>
-          <Box backgroundColor='green.200' flexGrow={1} height='100%' borderRadius={10} mr={3}></Box>
+          <Box backgroundColor='green.200' flexGrow={1} height='100%' borderRadius={10} mr={3} align='center' justify='center'>
+            <Flex width='100%' justify='center' align='center' direction='column' p={1}>
+              <Text fontWeight={600}>Avoid using names or words found in the dictionary</Text>
+              <Text>jack</Text>
+              <Text>orange</Text>
+              <Text>football</Text>
+            </Flex>
+          </Box>
           <Flex width='100px' height='90%' borderRadius='full' border='1px solid grey' align='center' justify='center' display={{base:'none',md:'flex'}}>4</Flex>
         </Flex>
         <Flex height='120px' width='100%' align='center'>
           <Flex width='100px' height='90%' borderRadius='full' border='1px solid grey' align='center' justify='center' mr={3} display={{base:'none',md:'flex'}}>5</Flex>
-          <Box backgroundColor='blue.200' flexGrow={1} height='100%' borderRadius={10}></Box>
+          <Box backgroundColor='blue.200' flexGrow={1} height='100%' borderRadius={10} align='center' justify='center'>
+            <Flex width='100%' justify='center' align='center' direction='column' p={1}>
+              <Text fontWeight={600}>Avoid using names or words found in the dictionary</Text>
+              <Text>jack</Text>
+              <Text>orange</Text>
+              <Text>football</Text>
+            </Flex>
+          </Box>
         </Flex>
       </Stack>
 
