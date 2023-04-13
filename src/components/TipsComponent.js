102cc486bf8d949432628ed65f5df1e8ecf794fe
Umar Ahmed <108314943+umar-ahmed1@users.noreply.github.com>
2023-04-12 20:31:57 -0600

    some mobile styling

--- src/components/TipsComponent.js
+++ src/components/TipsComponent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Box, Flex, Stack, Text } from '@chakra-ui/react';
 
 export default function TipsComponent() {
@@ -13,7 +13,7 @@ export default function TipsComponent() {
     >
       <Flex
         direction="column"
-        width={{ base: '70%', sm: '60%', md:'50%' }}
+        width={{ base: '85%', sm: '70%', md: '50%' }}
         align="center"
         justify="center"
       >
@@ -36,68 +36,205 @@ export default function TipsComponent() {
           </Text>
         </Flex>
 
-
-      <Stack width='100%' spacing={5} fontSize={{base:'13px',sm:'14px',md:'15px'}}>
-        <Flex height='120px' width='100%' align='center'>
-          <Flex cursor='pointer' width='100px' height='90%' borderRadius='full' border='1px solid grey' align='center' justify='center' mr={3} display={{base:'none',md:'flex'}} fontWeight={700}>Why?</Flex>
-          <Box backgroundColor='red.200' flexGrow={1} height='100%' borderRadius={10} align='center' justify='center'>
-            <Flex width='100%' justify='center' align='center' direction='column' p={1}>
-              <Text fontWeight={700}>Don't use a common password</Text>
-              <Text>qwerty</Text>
-              <Text>apple</Text>
-              <Text>123456</Text>
+        <Stack
+          width="100%"
+          spacing={5}
+          fontSize={{ base: '13px', sm: '14px', md: '15px' }}
+        >
+          <Flex width="100%" align="center">
+            <Flex
+              cursor="pointer"
+              width={{base:'60px',md:"100px"}}
+              height={{base:'60px',md:"100px"}}
+              borderRadius="full"
+              border="1px solid grey"
+              align="center"
+              justify="center"
+              mr={{base:1,md:3}}
+              fontWeight={700}
+              _hover={{ backgroundColor: 'red.100', opacity:0.9}}
+            >
+              Why?
             </Flex>
-          </Box>
-        </Flex>
-        <Flex height='120px' width='100%' align='center'>
-          <Box backgroundColor='yellow.200' flexGrow={1} height='100%' borderRadius={10} mr={3} align='center' justify='center'>
-            <Flex width='100%' justify='center' align='center' direction='column' p={1}>
-              <Text fontWeight={700}>Don't include any personal information in your password</Text>
-              <Text>Tanaka2003</Text>
-              <Text>Oct15Tanaka</Text>
-              <Text>TanakaOct142003</Text>
+            <Box
+              backgroundColor="red.200"
+              flexGrow={1}
+              height="100%"
+              borderRadius={10}
+              align="center"
+              justify="center"
+            >
+              <Flex
+                width="100%"
+                justify="center"
+                align="center"
+                direction="column"
+                p={1}
+              >
+                <Text fontWeight={700}>Don't use a common password</Text>
+                <Text>qwerty</Text>
+                <Text>apple</Text>
+                <Text>123456</Text>
+              </Flex>
+            </Box>
+          </Flex>
+          <Flex width="100%" align="center">
+            <Box
+              backgroundColor="yellow.200"
+              flexGrow={1}
+              height="100%"
+              borderRadius={10}
+              mr={{base:1,md:3}}
+              align="center"
+              justify="center"
+            >
+              <Flex
+                width="100%"
+                justify="center"
+                align="center"
+                direction="column"
+                p={1}
+              >
+                <Text fontWeight={700}>
+                  Don't include any personal information in your password
+                </Text>
+                <Text>Tanaka2003</Text>
+                <Text>Oct15Tanaka</Text>
+                <Text>TanakaOct142003</Text>
+              </Flex>
+            </Box>
+            <Flex
+              cursor="pointer"
+              width={{base:'60px',md:"100px"}}
+              height={{base:'60px',md:"100px"}}
+              borderRadius="full"
+              border="1px solid grey"
+              align="center"
+              justify="center"
+              fontWeight={700}
+              _hover={{ backgroundColor: 'yellow.100', opacity:0.9}}
+            >
+              Why?
             </Flex>
-          </Box>
-          <Flex cursor='pointer' width='100px' height='90%' borderRadius='full' border='1px solid grey' align='center' justify='center' display={{base:'none',md:'flex'}} fontWeight={700}>Why?</Flex>
-        </Flex>
-        <Flex height='120px' width='100%' align='center'>
-          <Flex cursor='pointer' width='100px' height='90%' borderRadius='full' border='1px solid grey' align='center' justify='center' mr={3} display={{base:'none',md:'flex'}} fontWeight={700}>Why?</Flex>
-          <Box backgroundColor='orange.200' flexGrow={1} height='100%' borderRadius={10} align='center' justify='center'>
-            <Flex width='100%' justify='center' align='center' direction='column' p={1}>
-              <Text fontWeight={700}>Use passwords that include at least 8 letters, lower case, uppercase, numbers, and symbols</Text>
-              <Text>T@n@ka62@31</Text>
-              <Text>Cp$C3@W!nT#R@3</Text>
-              <Text>J@n3T!$Th3B3$TPr0f</Text>
+          </Flex>
+          <Flex width="100%" align="center">
+            <Flex
+              cursor="pointer"
+              width={{base:'60px',md:"100px"}}
+              height={{base:'60px',md:"100px"}}
+              borderRadius="full"
+              border="1px solid grey"
+              align="center"
+              justify="center"
+              mr={{base:1,md:3}}
+              fontWeight={700}
+              _hover={{ backgroundColor: 'orange.100', opacity:0.9}}
+            >
+              Why?
             </Flex>
-          </Box>
-        </Flex>
-        <Flex height='120px' width='100%' align='center'>
-          <Box backgroundColor='green.200' flexGrow={1} height='100%' borderRadius={10} mr={3} align='center' justify='center'>
-            <Flex width='100%' justify='center' align='center' direction='column' p={1}>
-              <Text fontWeight={700}>Do not reuse your passwords</Text>
-              <Text>Instagram: T@n@ka62@31</Text>
-              <Text>Twitter: J@n3T!$ThB3$TPr0f</Text>
-              <Text>Cp$C3@W1nT#R@3</Text>
+            <Box
+              backgroundColor="orange.200"
+              flexGrow={1}
+              height="100%"
+              borderRadius={10}
+              align="center"
+              justify="center"
+            >
+              <Flex
+                width="100%"
+                justify="center"
+                align="center"
+                direction="column"
+                p={1}
+              >
+                <Text fontWeight={700}>
+                  Use passwords that include at least 8 letters, lower case,
+                  uppercase, numbers, and symbols
+                </Text>
+                <Text>T@n@ka62@31</Text>
+                <Text>Cp$C3@W!nT#R@3</Text>
+                <Text>J@n3T!$Th3B3$TPr0f</Text>
+              </Flex>
+            </Box>
+          </Flex>
+          <Flex height="120px" width="100%" align="center">
+            <Box
+              backgroundColor="green.200"
+              flexGrow={1}
+              height="100%"
+              borderRadius={10}
+              mr={{base:1,md:3}}
+              align="center"
+              justify="center"
+            >
+              <Flex
+                width="100%"
+                justify="center"
+                align="center"
+                direction="column"
+                p={1}
+              >
+                <Text fontWeight={700}>Do not reuse your passwords</Text>
+                <Text>Instagram: T@n@ka62@31</Text>
+                <Text>Twitter: J@n3T!$ThB3$TPr0f</Text>
+                <Text>Cp$C3@W1nT#R@3</Text>
+              </Flex>
+            </Box>
+            <Flex
+              cursor="pointer"
+              width={{base:'60px',md:"100px"}}
+              height={{base:'60px',md:"100px"}}
+              borderRadius="full"
+              border="1px solid grey"
+              align="center"
+              justify="center"
+              fontWeight={700}
+              _hover={{ backgroundColor: 'green.100', opacity:0.9}}
+            >
+              Why?
             </Flex>
-          </Box>
-          <Flex cursor='pointer' width='100px' height='90%' borderRadius='full' border='1px solid grey' align='center' justify='center' display={{base:'none',md:'flex'}} fontWeight={700}>Why?</Flex>
-        </Flex>
-        <Flex height='120px' width='100%' align='center'>
-          <Flex cursor='pointer' width='100px' height='90%' borderRadius='full' border='1px solid grey' align='center' justify='center' mr={3} display={{base:'none',md:'flex'}} fontWeight={700}>Why?</Flex>
-          <Box backgroundColor='blue.200' flexGrow={1} height='100%' borderRadius={10} align='center' justify='center'>
-            <Flex width='100%' justify='center' align='center' direction='column' p={1}>
-              <Text fontWeight={700}>Avoid using names or words found in the dictionary</Text>
-              <Text>jack</Text>
-              <Text>orange</Text>
-              <Text>football</Text>
+          </Flex>
+          <Flex height="120px" width="100%" align="center">
+            <Flex
+              cursor="pointer"
+              width={{base:'60px',md:"100px"}}
+              height={{base:'60px',md:"100px"}}
+              borderRadius="full"
+              border="1px solid grey"
+              align="center"
+              justify="center"
+              mr={{base:1,md:3}}
+              fontWeight={700}
+              _hover={{ backgroundColor: 'blue.100', opacity:0.9}}
+            >
+              Why?
             </Flex>
-          </Box>
-        </Flex>
-      </Stack>
-
-
-
+            <Box
+              backgroundColor="blue.200"
+              flexGrow={1}
+              height="100%"
+              borderRadius={10}
+              align="center"
+              justify="center"
+            >
+              <Flex
+                width="100%"
+                justify="center"
+                align="center"
+                direction="column"
+                p={1}
+              >
+                <Text fontWeight={700}>
+                  Avoid using names or words found in the dictionary
+                </Text>
+                <Text>jack</Text>
+                <Text>orange</Text>
+                <Text>football</Text>
+              </Flex>
+            </Box>
+          </Flex>
+        </Stack>
       </Flex>
     </Flex>
-  )
+  );
 }
